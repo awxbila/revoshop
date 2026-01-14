90dda1e1fb2b2e53576dcdc145bae0e6eb748142
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2026-01-15 00:07:45 +0700

    dep

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { products } from "@/data/products";
+import { products } from "../../../data/products";
 
 export async function GET() {
   return NextResponse.json(products);
